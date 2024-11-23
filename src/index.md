5c076dcf1e922af1d0970804d720d2afa5b5c665
Saderra <hello@saderra.com>
2024-11-23 14:58:36 -0700

    the familie

--- src/index.md
+++ src/index.md
@@ -1,5 +1,5 @@
 ---
 title: The Familie
-description: The Description
-keywords: "child psychologist, gender-affirming care, gender-affirming therapist"
+description: An LGBTQIA+ Affirming Therapy Clinic, Serving 40 US States
+keywords: "lgbt therapy, gender affirming therapy, lgbt relationship therapy"
 ---
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,6 @@ module.exports = {
   theme: {
     container: {
 			center: true,
-			padding: {
-				DEFAULT: '.8rem',
-				sm: '1.5rem'
-			  },
 			screens: {
 				sm: "100%",
 				md: "100%",
@@ -16,10 +12,29 @@ module.exports = {
 				"2xl": "1540px"
     },
   },
-    
-    
-    
-    extend: {},
+    extend: {
+		colors: {
+			familie: {
+				DEFAULT: '#fffefa',
+				'50': '#f2f1ed',
+				'100': '#e5e5e1',
+				'150': '#d9d8d4',
+				'200': '#cccbc8',
+				'300': '#b3b2af',
+				'400': '#999896',
+				'500': '#807f7d',
+				'600': '#666664',
+				'700': '#4d4c4b',
+				'800': '#333332',
+				'900': '#1a1919'
+
+
+			}
+		},
+
+
+
+	},
   },
   variants: {
 		display: ['responsive', 'group-hover', 'group-focus'],
